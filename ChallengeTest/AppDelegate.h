a35e19cf7c3f23e8aead23692cc9e8a3ce122c82
YinYanhui <yinyanhui@YinYanhuideMacBook-Pro.local>
2014-11-13 19:46:04 +0800

    Initial Commit

---
+++ ChallengeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChallengeTest
+//
+//  Created by YinYanhui on 14-11-13.
+//  Copyright (c) 2014年 YinYanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChallengeTestTests/ChallengeTestTests.m
@@ -0,0 +1,34 @@
+//
+//  ChallengeTestTests.m
+//  ChallengeTestTests
+//
+//  Created by YinYanhui on 14-11-13.
+//  Copyright (c) 2014年 YinYanhui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChallengeTestTests : XCTestCase
+
+@end
+
+@implementation ChallengeTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
